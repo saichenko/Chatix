cd93fa3889b9d1d08d1b0262e50002f3450249b6
VladSaichenko <reallinuxforlinux@gmail.com>
2021-07-31 18:48:15 +0700

    Update BaseModel

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,12 +1,21 @@
-from django_extensions.db.models import TimeStampedModel
+from django.db import models
+from django.utils.translation import gettext as _
 
 
-class BaseModel(TimeStampedModel):
+class BaseModel(models.Model):
     """Base model for apps' models.
 
-    This class adds to models created and modified fields
-
+    This class adds to models `created_at` and `modified_at` fields.
     """
+
+    created_at = models.DateTimeField(
+        verbose_name=_("created at"),
+        auto_now_add=True
+    )
+    modified_at = models.DateTimeField(
+        verbose_name=_("modified at"),
+        auto_now=True
+    )
 
     class Meta:
         abstract = True
